6b5809ce1ff7f5709e8bd2b6f509395c48438ea9
Joel Tong <me@joeltong.org>
2015-03-26 18:07:25 +0800

    pushed ver 1 of algorithm

---
+++ src/test/java/Tree2BinaryTreeTest.java
@@ -0,0 +1,9 @@
+import junit.framework.TestCase;
+
+public class Tree2BinaryTreeTest extends TestCase {
+
+  public void testLoadFile() throws Exception {
+    new Tree2BinaryTree().loadFile("testData.txt");
+
+  }
+}
